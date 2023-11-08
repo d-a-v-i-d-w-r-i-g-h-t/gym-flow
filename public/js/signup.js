ea8e5ac9f2ddbeee47bc43418fbd93aea589066f
DistantDig <jameswaller711@gmail.com>
2023-11-08 15:23:25 -0800

    added missing part of api call for signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -9,7 +9,7 @@ const signupFormHandler = async (event) => {
   
     // Api call to create a new user
     if (username && email && password) {
-      const response = await fetch('/api/users/', {
+      const response = await fetch('/api/users/signup', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
