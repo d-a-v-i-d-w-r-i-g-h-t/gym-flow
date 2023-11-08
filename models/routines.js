fabe855ac39bf2a65a4f40cbf2c3801ec694c3d3
Mike <mreickcastillero@gmail.com>
2023-11-08 00:03:00 -0800

    added models, seeds, and db

---
+++ models/routines.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection')
+
+const Users = require('./users')
+
+class Routines extends Model { }
+
+Routines.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            notNull: true,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            notNull: true,
+            unique: true,
+        },
+        share: {
+            type: DataTypes.BOOLEAN,
+            notNull: true,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            notNull: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            notNull: true,
+            references: {
+                model: Users,
+                key: 'id'
+            }
+        },
+    },
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Routines',
+    }
+);
+
+module.exports = Routines;
