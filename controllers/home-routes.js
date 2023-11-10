2ccd7865469b228e098138276d98019289126992
Mike <mreickcastillero@gmail.com>
2023-11-09 21:16:01 -0800

    adjusted handlebars while adding partial, placed in date newest filter page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,6 +36,30 @@ router.get('/discover', async (req, res) => {
     }
 });
 
+router.get('/discover/newest', async (req, res) => {
+    try {
+        const routinesdb = await Routine.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['user_name'],
+                },
+                {
+                    model: Exercise,
+                    attributes: ['id', 'name', 'weight', 'reps']
+                }
+            ],
+            order: [['date_created', 'DESC']]
+        });
+
+        const routines = routinesdb.map((routine) => routine.get({ plain: true }));
+
+        res.render('discover-newest', { routines }); // Assuming 'discover' is your view template
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.get('/profile', async (req,res) => {
     try{
         const profile = true;
