07397ce9cd66d86f97cecd3ed8334d13c43194c2
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-11 16:26:14 -0800

    Move like post route to routines/like/:routineId

--- controllers/api/like-routes.js
+++ controllers/api/like-routes.js
@@ -2,22 +2,7 @@ const router = require('express').Router();
 const { Like } = require('../../models');
 const withAuth = require('../../utils/authorize');
 
-// POST route to add a like
-router.post('/:routineId', withAuth, async (req, res) => {
-  try {
-    const userId = req.session.user_id;
-    const routineId = req.params.routineId;
 
-    const newLike = await Like.create({
-      user_id: userId,
-      routine_id: routineId,
-    });
-
-    res.status(201).json({ success: true, data: newLike });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
-  }
-});
 
 
 
--- controllers/api/routine-routes.js
+++ controllers/api/routine-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Routine, Exercise, User } = require('../../models');
+const { Routine, Exercise, User, Like } = require('../../models');
 const withAuth = require('../../utils/authorize');
 
 
@@ -84,6 +84,23 @@ router.delete('/:id', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// POST route to add a like
+router.post('/like/:routineId', withAuth, async (req, res) => {
+    try {
+      const userId = req.session.user_id;
+      const routineId = req.params.routineId;
+  
+      const newLike = await Like.create({
+        user_id: userId,
+        routine_id: routineId,
+      });
+  
+      res.status(201).json({ success: true, data: newLike });
+    } catch (err) {
+      res.status(500).json({ success: false, error: err.message });
+    }
+  });
 
 
 module.exports = router;
