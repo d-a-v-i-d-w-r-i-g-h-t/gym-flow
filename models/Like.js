4f5ff444504e39a19898493dbacd79a4ae7c6f4f
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-11 16:05:20 -0800

    Add Like.js model

---
+++ models/Like.js
@@ -0,0 +1,45 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Like extends Model {}
+
+Like.init(
+  {
+    id: {
+      type: Datatypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    routine_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    }
+    createdAt: {
+      type: Datatypes.DATE,
+      allowNull: false,
+        defaultValue: Datatypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'like',
+  }
+);
+
+module.exports = Like;
