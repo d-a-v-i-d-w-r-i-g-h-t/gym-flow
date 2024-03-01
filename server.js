06420a5805fc2309281c40dd1d00e60e5e9c0081
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2024-03-01 14:29:38 -0800

    Add SESSION_SECRET environment variable

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESSION_SECRET || 'Super secret secret',
   resave: false,
   saveUninitialized: true,
   cookie: { 
